6203deddbf1fe95075fc67eb159fd46936c0c4b1
edgararthur <gilfoyle.arthur@protonmail.com>
2023-12-19 11:14:13 +0000

    custom screen for navigation

---
+++ Main.tsx
@@ -0,0 +1,13 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+import Home from './src/screens/Home'
+
+export default class Main extends Component {
+	render() {
+		return (
+			<Home />
+		)
+	}
+}
+
+const styles = StyleSheet.create({})
